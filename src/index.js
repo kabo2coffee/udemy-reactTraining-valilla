b81bb90d7c0f9bb26bec328c983e1182b8df8d5a
kabo2coffee <99697002+kabo2coffee@users.noreply.github.com>
2022-02-20 19:48:06 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import "./styles.css";
+const info = {
+  name: "知也",
+  age: 26
+};
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const message = `私の名前は${info.name}です。年齢は${info.age}です`;
+console.log(message);
+
+const { name, age } = info;
+const message2 = `私の名前は${name}です。年齢は${age}です`;
+console.log(message2);
+
+const infoArray = ["知也", 26];
+const massage3 = `私の名前は${infoArray[0]}です。年齢は${infoArray[1]}です`;
+console.log(message);
+
+const [name4, age4] = infoArray;
+const massage4 = `私の名前は${name4}です。年齢は${age4}です`;
+console.log(message);
