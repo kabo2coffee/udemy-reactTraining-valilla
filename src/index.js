24daa1aa5458139516712376c423534f6e6a8342
kabo2coffee <99697002+kabo2coffee@users.noreply.github.com>
2022-02-21 08:43:31 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -17,3 +17,47 @@ console.log(message);
 const [name4, age4] = infoArray;
 const massage4 = `私の名前は${name4}です。年齢は${age4}です`;
 console.log(message);
+
+/**
+ * デフォルト値
+ */
+const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
+sayHello();
+
+/**
+ * スプレッド構文
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー,結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7)
+
+// 参照コピーなので元配列も影響を受ける
+// const arr8 =  arr4;
+// console.log(arr8);
+// arr8[0] = 100;
+// console.log(arr8[0])
+// console.log(arr4[0])
